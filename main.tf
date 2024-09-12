3087235fe2d316bb11cb34a6ebeb36e00a9a74a9
StamatisChr <166624731+StamatisChr@users.noreply.github.com>
2024-09-12 15:49:21 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami             = "ami-03cc8375791cb8bcf"
-  instance_type   = "t3.micro"
+  instance_type   = "t2.micro"
 
   tags = {
     Name = "test-for-deletion"
